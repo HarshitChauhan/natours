a2e9a80ee666e870bd8bb988aa26e61075c011e0
HarshitChauhan <harshitkumar2503@gmail.com>
2022-09-07 16:30:24 +0530

    refactore code in better way

--- app.js
+++ app.js
@@ -8,32 +8,32 @@ app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-
 // Get All Tours
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
+const getAllTours = (req, res) => {
+res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+        tours
+    }
 })
-// Get Tour by ID
-app.get('/api/v1/tours/:id', (req, res) => {
-    const id = req.params.id * 1; // string to int conversion
-    const tour = tours.find(t => t.id === id);
+};
 
-    // tour not found
-    if(!tour){
-        return (
-            res.status(404).json({
-                status: 'failed',
-                message: `No tour found with id: ${id}`
-            })
+// Get Tour by ID
+const getTourById = (req, res) => {
+const id = req.params.id * 1; // string to int conversion
+const tour = tours.find(t => t.id === id);
+
+// tour not found
+if(!tour){
+    return (
+        res.status(404).json({
+            status: 'failed',
+            message: `No tour found with id: ${id}`
+        })
         );
     }
-
+    
     // on success
     res.status(200).json({
         status: 'success',
@@ -41,22 +41,23 @@ app.get('/api/v1/tours/:id', (req, res) => {
             tour
         }
     })
-})
-// Patch Tour details by ID
-app.patch('/api/v1/tours/:id', (req, res) => {
-    const id = req.params.id * 1; // string to int conversion
-    const tour = tours.find(t => t.id === id);
+};
 
-    // tour not found
-    if(id >= tours.length ){
-        return (
-            res.status(404).json({
-                status: 'failed',
-                message: `No tour found with id: ${id}`
-            })
+// Update Tour details by ID
+const updateTour = (req, res) => {
+const id = req.params.id * 1; // string to int conversion
+const tour = tours.find(t => t.id === id);
+
+// tour not found
+if(id >= tours.length ){
+    return (
+        res.status(404).json({
+            status: 'failed',
+            message: `No tour found with id: ${id}`
+        })
         );
     }
-
+    
     // on success
     res.status(200).json({
         status: 'success',
@@ -64,32 +65,34 @@ app.patch('/api/v1/tours/:id', (req, res) => {
             tour: '<Updating Tour...>'
         }
     })
-})
+};
+
 // Delete Tour by ID
-app.delete('/api/v1/tours/:id', (req, res) => {
-    const id = req.params.id * 1; // string to int conversion
-    const tour = tours.find(t => t.id === id);
+const deleteTour = (req, res) => {
+const id = req.params.id * 1; // string to int conversion
+const tour = tours.find(t => t.id === id);
 
-    // tour not found
-    if(id >= tours.length ){
-        return (
-            res.status(404).json({
-                status: 'failed',
-                message: `No tour found with id: ${id}`
-            })
-        );
-    }
-
-    // on success
-    res.status(204).json({
-        status: 'success',
-        data: {
-            tour: '<Deleting Tour...>'
-        }
+// tour not found
+if(id >= tours.length ){
+return (
+    res.status(404).json({
+        status: 'failed',
+        message: `No tour found with id: ${id}`
     })
+    );
+}
+
+// on success
+res.status(204).json({
+    status: 'success',
+    data: {
+        tour: '<Deleting Tour...>'
+    }
 })
+};
+
 // Create a new Tour
-app.post('/api/v1/tours', (req, res) => {
+const createNewTour = (req, res) => {
     console.log((req.body));
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id: newId}, req.body);
@@ -102,8 +105,26 @@ app.post('/api/v1/tours', (req, res) => {
             }
         })
     });
-})
+};
 
+/*
+app.get('/api/v1/tours', getAllTours);
+app.post('/api/v1/tours', createNewTour);
+app.get('/api/v1/tours/:id', getTourById);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
+*/
+
+{/*-----Refactoring routes in better way-----*/}
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createNewTour);
+
+app.route('/api/v1/tours/:id')
+    .get(getTourById)
+    .patch(updateTour)
+    .delete(deleteTour);
+    
 app.listen(port,()=>{
     console.log(`App server running on port ${port}!`);
 });
