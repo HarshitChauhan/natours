c2049c02f6b18457ab85c85a13201e807dff2c2c
HarshitChauhan <harshitkumar2503@gmail.com>
2022-09-11 23:58:43 +0530

    saving sample data to MongoDB

--- server.js
+++ server.js
@@ -16,6 +16,38 @@ mongoose.connect(connectionString, {
   console.log('DB Connected!');
 });
 
+// Mongo Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'Name is a required field!'] // <required: [status, 'error message']>
+  },
+  duration: Number, // if no other property to define, simply specify <fieldName: dataType>
+  rating: {
+    type: Number,
+    default: 4.3
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is a required field!']
+  },
+})
+
+const Tour = mongoose.model('Tour', tourSchema); // created a Tour out of this specified schema
+ // creating instance of Tour object
+const testTour = new Tour ( {
+  name : 'The Forest',
+  duration: 10,
+  price : 497
+})
+ // saving tours to MongoDB
+ testTour.save().then( doc => {
+  console.log (doc)
+ }).catch(err => {
+   console.log('ERROR💥: ', err)
+  })
+
 // Starting Server
 const port = process.env.PORT;
 app.listen(port, () => {
