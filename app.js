2570cdb58a6a981d3d1bdec73c44cef351c49dfd
HarshitChauhan <harshitkumar2503@gmail.com>
2022-09-21 22:39:52 +0530

    Error handling (Global and custom way)

--- app.js
+++ app.js
@@ -24,4 +24,41 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Error Handling: Route not found
+app.all ('*', (req, res, next) => {
+  // custom handler //
+//   res.status(404).json({
+//     status: 'failed',
+//     timeStamp: req.requestTime,
+//     errorDetails: {
+//         reason: "ROUTE:NOT_FOUND",
+//         message: `Can't find ${req.originalUrl} on this server !`
+//     }
+//  });
+
+  // using Global Error Handler //
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'failed';
+  err.statusCode = 404;
+  err.reason = "ROUTE:NOT_FOUND";
+  err.timeStamp = req.requestTime;
+
+  next(err);
+});
+
+// Global Error Handler
+app.use((err, req, res, next)=>{
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'failed';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    timeStamp: err.timeStamp,
+    errorDetails: {
+        reason: err.reason,
+        message: err.message
+    }
+ });
+})
+
 module.exports = app;
