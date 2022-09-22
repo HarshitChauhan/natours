cff465113c0ad3a0934ceccd82628f31538a66c3
HarshitChauhan <harshitkumar2503@gmail.com>
2022-09-22 20:20:32 +0530

    ErrorHandling: catchAsync

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,11 @@
 module.exports = (err, req, res, next)=>{
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'failed';
+    err.timeStamp = req.requestTime || new Date().toISOString();
   
     res.status(err.statusCode).json({
       status: err.status,
-      timeStamp: req.requestTime,
+      timeStamp: err.timeStamp,
       errorDetails: {
           reason: err.reason, // need to take care of [error reason] later
           message: err.message
