cc1e508fd2daea52e7f396a9d42c7768de2e0252
HarshitChauhan <harshitkumar2503@gmail.com>
2022-09-22 22:51:08 +0530

    ErrorHandling: Dev vs Prod logging

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,51 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        httpStatus: err.statusCode,
+        timeStamp: err.timeStamp,
+        errorDetails: {
+            //reason: err.reason, // need to take care of [error reason] later
+            message: err.message,
+            stack: err.stack
+        }
+    });
+}
+const sendErrorProd = (err, res) => {
+    // Operational errors are trusted, can show details to client
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            timeStamp: err.timeStamp,
+            errorDetails: {
+                // reason: err.reason, // need to take care of [error reason] later
+                message: err.message
+            }
+        });
+    }
+    // Programming or other unknown errors should not be populated to client
+    else {
+        // 1) Log error to console
+        console.error('ERROR 💥:', err);
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'Server error',
+            message: 'Something went wrong!'
+        })
+
+    }
+
+}
+
 module.exports = (err, req, res, next)=>{
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'failed';
     err.timeStamp = req.requestTime || new Date().toISOString();
-  
-    res.status(err.statusCode).json({
-      status: err.status,
-      timeStamp: err.timeStamp,
-      errorDetails: {
-          reason: err.reason, // need to take care of [error reason] later
-          message: err.message
-      }
-   });
+
+    // different format of error for each ENV
+    if(process.env.NODE_ENV === 'dev'){
+        sendErrorDev(err, res);
+    }else if(process.env.NODE_ENV === 'prod'){
+        sendErrorProd(err, res);
+    } 
   }
