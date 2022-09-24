b2e9bf5ae6b56e3bf03a7d2b6461230bde7f03d2
HarshitChauhan <harshitkumar2503@gmail.com>
2022-09-24 13:17:45 +0530

    ErrorHandling:unhandledRejection&UncaughtException

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// ErrorHandling: catching uncaught_exception(programming errors) before starting server
+process.on('uncaughtException' , err => {
+  console.log('UNCAUGHT_EXCEPTION: Shutting down the application...');
+  console.error({
+    status: '💥 ERROR 💥',
+    timeStamp: new Date().toISOString(),
+    errorDetails: {
+      reason: err.name,
+      message: err.message
+    }
+  });
+  process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 const app = require('./app');
 
@@ -18,6 +32,22 @@ mongoose.connect(connectionString, {
 
 // Starting Server
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App server running on port ${port}!`);
+});
+
+// ErrorHandling: Outside errors of Node/Express, Unhandled Rejections
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED_REJECTION: Shutting down the server...');
+  console.error({
+    status: '💥 ERROR 💥',
+    timeStamp: new Date().toISOString(),
+    errorDetails: {
+      reason: err.name,
+      message: err.message
+    }
+  });
+  server.close(() => { //server.close() first closes the server then exists the process
+    process.exit(1);
+  });
 });
